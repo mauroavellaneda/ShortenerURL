a2bde3dad394e170dd3e11267f58faf3b19c5360
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2021-01-30 21:00:51 -0300

    Adding environmental variables and connecting project to mongo compass

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const path = require("path");
 const routes = require("./router");
 const bodyParser = require("body-parser");
 
+require("dotenv").config({ path: "var.env" });
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/URLShortener", {
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
 });
-
 
 app.set("view engine", "pug");
 
@@ -23,4 +24,9 @@ app.use(express.static("public"));
 
 app.use("/", routes());
 
-app.listen(5000);
+const host = process.env.HOST || "0.0.0.0";
+const port = process.env.PORT || 5000
+
+app.listen(port, host, () => {
+  console.log("app is working")
+});
